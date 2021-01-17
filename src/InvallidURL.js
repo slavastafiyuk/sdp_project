c1651f95085ebb987e89c7e8b22deec67cf6e85c
slavastafiyuk <slavast@outlook.pt>
2021-01-17 16:12:22 +0000

    update

--- src/InvallidURL.js
+++ src/InvallidURL.js
@@ -3,7 +3,7 @@ import React from 'react';
 function InvalidPath() {
     return(
         <div>
-            <h2>A URL que voce indicou é invalido</h2>
+            <h2>A URL que voce indicou é invalida</h2>
         </div>
     )
 }
